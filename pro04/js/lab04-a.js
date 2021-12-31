7f9b84558553aff63e935bd3a7ee7540c290815f
luotanlj <88665738+luotanlj@users.noreply.github.com>
2021-12-31 20:20:19 +0800

    Add files via upload

---
+++ pro04/js/lab04-a.js
@@ -0,0 +1,150 @@
+"use strict";
+
+var canvas;
+var gl;
+
+var points = [];
+var colors = [];
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+
+var axis = 0;
+var theta = [0, 0, 0];
+
+var thetaLoc;
+
+var dicx = -1;
+var dicLocx;
+var dicy = -1;
+var dicLocy;
+var dicz = -1;
+var dicLocz;
+var dicflag = 1;
+
+var delay = 1000;
+
+window.onload = function initCube() {
+    canvas = document.getElementById("rtcb-canvas");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        alert("WebGL isn't available");
+    }
+
+    makeCube();
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    gl.enable(gl.DEPTH_TEST);
+
+    // load shaders and initialize attribute buffer
+    var program = initShaders(gl, "rtvshader", "rtfshader");
+    gl.useProgram(program);
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    thetaLoc = gl.getUniformLocation(program, "theta");
+    gl.uniform3fv(thetaLoc, theta);
+
+    dicLocx = gl.getUniformLocation(program, "dicx");
+    gl.uniform1f(dicLocx, dicx);
+    dicLocy = gl.getUniformLocation(program, "dicy");
+    gl.uniform1f(dicLocy, dicy);
+    dicLocz = gl.getUniformLocation(program, "dicz");
+    gl.uniform1f(dicLocz, dicz);
+
+    document.getElementById("xbutton").onclick = function () {
+        axis = xAxis;
+    }
+
+    document.getElementById("ybutton").onclick = function () {
+        axis = yAxis;
+    }
+
+    document.getElementById("zbutton").onclick = function () {
+        axis = zAxis;
+    }
+
+    document.getElementById("dic").onclick = function () {
+        // dic = -dic;
+        if(dicflag===xAxis)dicx = -dicx;
+        else if(dicflag===yAxis)dicy = -dicy;
+        else dicz = -dicz;
+        console.log("111"+dic)
+    }
+
+    render();
+}
+
+function makeCube() {
+    var vertices = [
+        glMatrix.vec4.fromValues(-0.5, -0.5, 0.5, 1.0),
+        glMatrix.vec4.fromValues(-0.5, 0.5, 0.5, 1.0),
+        glMatrix.vec4.fromValues(0.5, 0.5, 0.5, 1.0),
+        glMatrix.vec4.fromValues(0.5, -0.5, 0.5, 1.0),
+        glMatrix.vec4.fromValues(-0.5, -0.5, -0.5, 1.0),
+        glMatrix.vec4.fromValues(-0.5, 0.5, -0.5, 1.0),
+        glMatrix.vec4.fromValues(0.5, 0.5, -0.5, 1.0),
+        glMatrix.vec4.fromValues(0.5, -0.5, -0.5, 1.0),
+    ];
+
+    var vertexColors = [
+        glMatrix.vec4.fromValues(0.0, 0.0, 0.0, 1.0),
+        glMatrix.vec4.fromValues(1.0, 0.0, 0.0, 1.0),
+        glMatrix.vec4.fromValues(1.0, 1.0, 0.0, 1.0),
+        glMatrix.vec4.fromValues(0.0, 1.0, 0.0, 1.0),
+        glMatrix.vec4.fromValues(0.0, 0.0, 1.0, 1.0),
+        glMatrix.vec4.fromValues(1.0, 0.0, 1.0, 1.0),
+        glMatrix.vec4.fromValues(0.0, 1.0, 1.0, 1.0),
+        glMatrix.vec4.fromValues(1.0, 1.0, 1.0, 1.0)
+    ];
+
+    var faces = [
+        1, 0, 3, 1, 3, 2, //正
+        2, 3, 7, 2, 7, 6, //右
+        3, 0, 4, 3, 4, 7, //底
+        6, 5, 1, 6, 1, 2, //顶
+        4, 5, 6, 4, 6, 7, //背
+        5, 4, 0, 5, 0, 1  //左
+    ];
+
+    for (var i = 0; i < faces.length; i++) {
+        points.push(vertices[faces[i]][0], vertices[faces[i]][1], vertices[faces[i]][2]);
+        console.log(faces[i][0]);
+        console.log("i="+i+"  "+faces[i][0]+" "+faces[i][1]+" "+faces[i][2]);
+
+        colors.push(vertexColors[Math.floor(i / 6)][0], vertexColors[Math.floor(i / 6)][1], vertexColors[Math.floor(i / 6)][2], vertexColors[Math.floor(i / 6)][3]);
+    }
+}
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    theta[axis] += 0.1;
+    gl.uniform3fv(thetaLoc, theta);
+    gl.uniform1f(dicLocx, dicx);
+    gl.uniform1f(dicLocy, dicy);
+    gl.uniform1f(dicLocz, dicz);
+
+    gl.drawArrays(gl.TRIANGLES, 0, points.length / 3);
+
+    // requestAnimFrame(render);
+    setTimeout( function (){ requestAnimFrame( render ); }, 1);
+}
